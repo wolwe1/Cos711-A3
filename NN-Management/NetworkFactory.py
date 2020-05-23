4c14a5894f7d6613637c16db1fdf9777917f24d1
Jarrod <u17112631@tuks.co.za>
2020-05-23 18:34:44 +0200

    initial
    Signed-off-by: Jarrod <u17112631@tuks.co.za>

---
+++ NN-Management/NetworkFactory.py
@@ -0,0 +1,9 @@
+class NetworkFactory:
+
+    def CreateNetwork(networkName):
+        network = None
+
+        if networkName == "LSTM"    :
+            return network
+        elif networkName == "CNN"   :
+            return network
